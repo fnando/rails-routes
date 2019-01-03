854d75b9603a7b377d281fff8635d8049ea7b1d9
UzxMx <mingxiangxue@gmail.com>
2019-01-03 11:58:52 +0800

    Make sure the routes file list is ordered
    Because among the matched routes, the one that is created before the other will be of the highest priority, so  we need to be clear about which file is loaded before the other file in order to avoid different behavior on different platform.

--- lib/rails-routes/railtie.rb
+++ lib/rails-routes/railtie.rb
@@ -2,7 +2,7 @@ module RailsRoutes
   class Railtie < Rails::Railtie
     initializer "rails_routes" do
       app = Rails.application
-      routes = Dir[Rails.root.join("config/routes/**/*.rb").to_s]
+      routes = Dir[Rails.root.join("config/routes/**/*.rb").to_s].sort
       app.routes_reloader.paths.unshift(*routes)
       app.config.paths["config/routes.rb"].concat(routes)
     end
